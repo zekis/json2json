b966efd43e08463c8ed5ec66d3b32052f11cf803
Zeke Tierney <zeke.tierney@sgcontrols.com.au>
2024-04-18 13:56:06 +0800

    py and js templates

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ document.getElementById("open-json-btn").addEventListener("change", function(eve
     reader.onload = function(e) {
         const content = e.target.result;
         document.getElementById("open-filepath").value = file.name;
+        //get the docname from the filename
+        let docname = file.name.split('.')[0];
+        document.getElementById("save-docname").value = docname;
         try {
             const json = JSON.parse(content);
             const { fields, permissions, field_order, ...restOfJson } = json; // Destructure JSON to separate fields, permissions and field_order
@@ -32,7 +35,9 @@ document.getElementById("open-json-right-btn").addEventListener("change", functi
     const reader = new FileReader();
     reader.onload = function(e) {
         const content = e.target.result;
-        document.getElementById("save-filepath").value = file.name;
+        //document.getElementById("save-filepath").value = file.name;
+        let docname = file.name.split('.')[0];
+        document.getElementById("save-docname").value = docname;
         try {
             const json = JSON.parse(content);
             const { fields, permissions, field_order, ...restOfJson } = json; 
@@ -57,7 +62,7 @@ document.getElementById("open-json-right-btn").addEventListener("change", functi
 });
 
 document.getElementById("save-json-btn").addEventListener("click", function() {
-    const filepath = "exported.json"; 
+    const filepath = document.getElementById("save-docname").value + ".json"; 
     const selectedProperties = collectSelectedProperties();
     const selectedFields = collectSelectedJSON();
     const selectedPermissions = collectSelectedPermissions();
@@ -73,3 +78,69 @@ document.getElementById("save-json-btn").addEventListener("click", function() {
     a.download = filepath;
     a.click();
 });
+
+document.getElementById("save-python-btn").addEventListener("click", function() {
+    const filepath = document.getElementById("save-docname").value + ".py"; 
+    let pyContent = createPythonTemplate(document.getElementById("save-docname").value);
+
+    let pyBlob = new Blob([pyContent], {type: "text/plain"});
+    let pyUrl = URL.createObjectURL(pyBlob);
+
+    // Create a link and click it to start the download
+    let pyLink = document.createElement("a");
+    pyLink.href = pyUrl;
+    pyLink.download = filepath
+    pyLink.click();
+
+});
+
+document.getElementById("save-js-btn").addEventListener("click", function() {
+    const filepath = document.getElementById("save-docname").value + ".js"; 
+    let jsContent = createJSTemplate(document.getElementById("save-docname").value);
+
+    let jsBlob = new Blob([jsContent], {type: "text/plain"});
+    let jsUrl = URL.createObjectURL(jsBlob);
+
+    // Create a link and click it to start the download
+    let jsLink = document.createElement("a");
+    jsLink.href = jsUrl;
+    jsLink.download = filepath
+    jsLink.click();
+});
+
+
+// Create python template
+function createPythonTemplate(docName) {
+    // Convert the document name to the appropriate format
+    let className = docName.split('_')
+                           .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+                           .join('');
+    
+    let pyContent = `# Copyright (c) 2024, SG Controls and contributors
+    # For license information, please see license.txt
+
+    # import frappe
+    from frappe.model.document import Document
+
+    class ${className}(Document):
+        pass`; 
+    return pyContent;
+}
+
+// Create JavaScript template
+function createJSTemplate(docName) {
+    // Convert the document name to the appropriate format
+    let className = docName.split('_')
+                           .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+                           .join('');
+    
+    let jsContent = `// Copyright (c) 2024, SG Controls and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('${className}', {
+    doc_type: function(frm) {
+        console.log(frm.doc.doc_type);	
+    }
+});`; 
+    return jsContent;
+}
